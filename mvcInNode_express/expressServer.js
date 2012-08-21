4a2d15652aba9d12a1a6faa998ce28c6c6b34dec
chronoer <scentsome@gmail.com>
2012-08-21 12:32:08 +0800

    add index and send get route

--- mvcInNode_express/expressServer.js
+++ mvcInNode_express/expressServer.js
@@ -1,8 +1,24 @@
 var express = require("express");
 var app = express();
 app.get('/', function(req, res) { 
-	res.send("Hello Express Server");
+	res.send("<h1>Hello Express Server</h1>");
 	res.end();
 }); 
-
+app.get('/index', function(req, res) { 
+	res.send("<h1>Hello Express Server</h1>");
+	res.end();
+});
+app.get('/send', function(req, res) { 
+	var message = "<h1>Welcome to Express Server</h1>";
+	var name = req.query.name;
+	var age = req.query.age;
+	if (name) {
+		message = message +"<p> Hello "+name+"</p>";
+	}
+	if (age) {
+		message = message +"<p> You are "+age+" years old.</p>";
+	}
+	res.send(message);
+	res.end();
+}); 
 app.listen(8800);
