c43f9f1f9fe069c61a69f4c6025052360c3779f3
chronoer <scentsome@gmail.com>
2012-08-21 18:04:50 +0800

    comment edit button & change the class of view controller in storyboard

--- htmlChart/PieDemo/PieDemo/MasterViewController.m
+++ htmlChart/PieDemo/PieDemo/MasterViewController.m
@@ -27,7 +27,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    self.navigationItem.leftBarButtonItem = self.editButtonItem;
+//    self.navigationItem.leftBarButtonItem = self.editButtonItem;
 
     UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
     self.navigationItem.rightBarButtonItem = addButton;
