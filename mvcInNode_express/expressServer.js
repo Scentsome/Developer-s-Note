247c598fd1f727334d71720a3db9a44e9137002e
chronoer <scentsome@gmail.com>
2012-08-21 12:54:16 +0800

    add form client.html

--- mvcInNode_express/expressServer.js
+++ mvcInNode_express/expressServer.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+app.use(express.bodyParser());
 app.get('/', function(req, res) { 
 	res.send("<h1>Hello Express Server</h1>");
 	res.end();
@@ -21,4 +22,8 @@ app.get('/send', function(req, res) {
 	res.send(message);
 	res.end();
 }); 
+app.post('/formData', function(req, res) { 
+	res.send("<h1>Hello "+ req.body.username+"</h1>");
+	res.end();
+});
 app.listen(8800);
