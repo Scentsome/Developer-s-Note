a4900d9645c08f1530bbd27469bbbdb3f7021c54
chronoer <scentsome@gmail.com>
2012-08-21 19:21:44 +0800

    add test button

--- htmlChart/PieDemo/PieDemo/GraphViewController.m
+++ htmlChart/PieDemo/PieDemo/GraphViewController.m
@@ -28,15 +28,39 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(testJS)];
+    self.navigationItem.rightBarButtonItem = addButton;
     NSString * fileURL = [[NSBundle mainBundle] pathForResource:@"graph" ofType:@"html" inDirectory:@"jqplot"];
     NSURL * url = [NSURL URLWithString:[fileURL stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
     NSURLRequest * urlRequest = [NSURLRequest requestWithURL:url];
     [myWebView loadRequest:urlRequest];
     myWebView.delegate = self;
+   
+   
+    
+    
 	// Do any additional setup after loading the view.
+}
+-(void) testJS{
+    [myWebView stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@"gotData('%@','%@');", @"hello", @"world"]];
 }
 -(void) webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error{
     NSLog(@"error r%@", error);
+}
+-(void) webViewDidFinishLoad:(UIWebView *)webView{
+    NSMutableArray * catArray = [NSMutableArray array];
+    NSMutableArray * valueArray = [NSMutableArray array];
+    [self.sums enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        [catArray addObject:key];
+        [valueArray addObject:obj];
+    }];
+ 
+    NSString * jsonArray = [[NSString alloc] initWithData:[NSJSONSerialization dataWithJSONObject:catArray options:NSJSONWritingPrettyPrinted error:NULL] encoding:NSUTF8StringEncoding];
+    NSString * jsonValue = [[NSString alloc] initWithData:[NSJSONSerialization dataWithJSONObject:valueArray options:NSJSONWritingPrettyPrinted error:NULL] encoding:NSUTF8StringEncoding];
+    NSLog(@"json Array %@", jsonArray);
+    NSLog(@"value array %@", jsonValue);
+    [myWebView stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@"gotData(%@,%@);",jsonArray ,jsonValue]];
 }
 - (void)viewDidUnload
 {
