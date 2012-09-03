5d8ee3dec4c1b00eaf7dc968e4f3a0820527a01f
chronoer <scentsome@gmail.com>
2012-09-03 11:19:21 +0800

    test remote server

--- jsonUpDown/HelloJson/HelloJson/GLViewController.h
+++ jsonUpDown/HelloJson/HelloJson/GLViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface GLViewController : UIViewController
+@interface GLViewController : UIViewController<NSURLConnectionDataDelegate>
 
 @end
--- jsonUpDown/HelloJson/HelloJson/GLViewController.m
+++ jsonUpDown/HelloJson/HelloJson/GLViewController.m
@@ -7,16 +7,26 @@
 //
 
 #import "GLViewController.h"
-
-@interface GLViewController ()
-
+#define Server @"http://127.0.0.1:8800/json"
+#define Remote @"http://scentsome.course.jit.su/json"
+@interface GLViewController (){
+    NSURLConnection * getConnection;
+    NSURLConnection * sendConnection;
+    
+}
+@property (strong) NSMutableData * upData;
+@property (strong) NSMutableData * downData;
 @end
 
 @implementation GLViewController
+@synthesize upData;
+@synthesize downData;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.downData = [NSMutableData data];
+    self.upData = [NSMutableData data];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -30,5 +40,42 @@
 {
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
+- (IBAction)sendJson:(id)sender {
+    self.upData = [NSMutableData data];
+    NSURL * jsonURL = [NSURL URLWithString:Remote];
+    NSMutableURLRequest * mRequest = [NSMutableURLRequest requestWithURL:jsonURL];
+    mRequest.HTTPMethod = @"POST";
+    [mRequest setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+    NSMutableData * postBody = [NSMutableData data];
+    [postBody appendData:[@"data=" dataUsingEncoding:NSUTF8StringEncoding]];
+    NSDictionary * jsonObject = [NSDictionary dictionaryWithObjectsAndKeys:@"michael",@"name",@"1234",@"password", nil];
+    [postBody appendData:[NSJSONSerialization dataWithJSONObject:jsonObject options:NSJSONWritingPrettyPrinted error:NULL]];
+    [mRequest setHTTPBody:postBody];
+    sendConnection = [NSURLConnection connectionWithRequest:mRequest delegate:self];
+}
+- (IBAction)getJson:(id)sender {
+    self.downData = [NSMutableData  data];
+    NSURL  * jsonURL = [NSURL URLWithString:[Remote stringByAppendingFormat:@"?data=%@",@"hello"]];
+    NSURLRequest * request = [NSURLRequest requestWithURL:jsonURL];
+    getConnection = [NSURLConnection connectionWithRequest:request delegate:self];
+}
+-(void) connection:(NSURLConnection *)connection didReceiveData:(NSData *)data{
+    if (connection == getConnection) {
+        [self.downData appendData:data];
+    }
+    if (connection == sendConnection) {
+        [self.upData appendData:data];
+    }
+}
 
+-(void) connectionDidFinishLoading:(NSURLConnection *)connection{
+    if (connection == getConnection) {
+        NSDictionary * jsonObject = [NSJSONSerialization JSONObjectWithData:self.downData options:NSJSONWritingPrettyPrinted error:NULL];
+        NSLog(@"Got json %@", jsonObject);
+    }
+    
+    if (connection == sendConnection) {
+        NSLog(@"Response : %@", [[NSString alloc] initWithData:self.upData encoding:NSUTF8StringEncoding]);
+    }
+}
 @end
