d08ee5e8267fe41d03c7e69a7aa2eaf4d91dd564
chronoer <scentsome@gmail.com>
2012-09-08 18:19:47 +0800

    init, add mongo url

---
+++ mongoDemo/accessMongo.js
@@ -0,0 +1,12 @@
+var mongoDB = require('mongodb')
+var generate_mongo_url = function(obj){
+	obj.hostname = (obj.hostname || 'localhost');
+	obj.port = (obj.port || 27017);
+	obj.db = (obj.db || 'test');
+	if(obj.username && obj.password){
+		return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
+	}
+	else{
+		return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
+	}
+}
