0cdf941c8d9e509556e576a79f4ed3a1d48fa80f
chronoer <scentsome@gmail.com>
2012-09-28 22:44:48 +0800

    create methods

---
+++ createMethodByC/MethodByC/MethodByC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MethodByC
+//
+//  Created by chronoer on 9/28/12.
+//  Copyright (c) 2012 chronoer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GLAppDelegate class]));
+    }
+}
