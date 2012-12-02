f53bbaafb643dda2568345b9f74f72bf2aeee2b6
Michael <scentsome@gmail.com>
2012-12-02 16:39:11 +0800

    add Car class

---
+++ NonARCTesting/NonARCTesting/Car.h
@@ -0,0 +1,13 @@
+//
+//  Car.h
+//  NonARCTesting
+//
+//  Created by chronoer on 12/12/2.
+//  Copyright (c) 2012年 DevelopersNote. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Car : NSObject
+
+@end
---
+++ NonARCTesting/NonARCTesting/Car.m
@@ -0,0 +1,19 @@
+//
+//  Car.m
+//  NonARCTesting
+//
+//  Created by chronoer on 12/12/2.
+//  Copyright (c) 2012年 DevelopersNote. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+-(id) newName{
+    return [NSString stringWithFormat:@"Honda"];
+}
+
+-(id) newname{
+    return [NSString stringWithFormat:@"Honda"];
+}
+@end
